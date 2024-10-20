d27d8bcbb4fe211b84d30bfebc0eb2ef3f66a076
Diego Oliveira <diegodsoliveira@gmail.com>
2024-10-20 12:13:05 -0300

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,24 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideLuxonDateAdapter } from '@angular/material-luxon-adapter';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(localePt);
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideLuxonDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt' },
+    provideHttpClient(),
+  ],
+};
